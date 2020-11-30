19022edc84743e301b9e34fd347700a33747b81e
Alan Silva <alansq16@gmail.com>
2020-11-30 15:35:58 -0500

    async changes and client & server timeouts
    - Making the methods async and calling them with awaits for better
    responsiveness.
    - Added the server and client timeout configurations to avoid operator
    freezes

--- app/misc.py
+++ app/misc.py
@@ -1,14 +1,24 @@
 import os
 import sys
 
-def require_env_var(name):
+def get_required_env_var(name, logger):
     """Returns value of a required environment variable. Fails if not found"""
     try:
         return os.environ[name]
     except KeyError:
-        print(f"{name} environment variable is required! Exiting.")
+        logger.error(f"{name} environment variable is required! Exiting.")
         sys.exit(1)
 
 def get_env_var_bool(name):
     """Gets value of environment variable as a boolean. If not 'true', returns False"""
     return os.getenv(name) == 'true'
+
+def get_env_var_int(name, default, logger):
+    """Gets value of environment variable as a boolean. If not a valid int,
+    returns the given default value"""
+    env_var = os.getenv(name, default)
+    try:
+        return int(env_var)
+    except TypeError:
+        logger.warning(f"""Expected an integer value for {name} and got {env_var}.
+Using default {default} instead""")
--- app/sidecar.py
+++ app/sidecar.py
@@ -1,23 +1,39 @@
+import os
+import asyncio
 import kopf
-from misc import require_env_var, get_env_var_bool
+from misc import get_required_env_var, get_env_var_bool, get_env_var_int
 from io_helpers import create_folder, write_file, delete_file
 
-# Check that the required environment variables are present before we start
-FOLDER = require_env_var('FOLDER')
-LABEL = require_env_var('LABEL')
+LABEL = None
 
 @kopf.on.startup()
 def startup_tasks(settings: kopf.OperatorSettings, logger, **_):
     """Perform all necessary startup tasks here. Keep them lightweight and relevant
     as the other handlers won't be initialized until these tasks are complete"""
+
+    # Check that the required environment variables are present before we start
+    folder = get_required_env_var('FOLDER', logger)
+    LABEL = get_required_env_var('LABEL', logger)
+
     # Replace the default marker with something less cryptic
     settings.persistence.finalizer = 'kopf.zalando.org/K8sSidecarFinalizerMarker'
+
+    # Set the client and service k8s API timeouts
+    # Very important! Without proper values, the operator may stop responding!
+    # See https://github.com/nolar/kopf/issues/585
+    client_timeout = get_env_var_int('WATCH_CLIENT_TIMEOUT', 60, logger)
+    server_timeout = get_env_var_int('WATCH_SERVER_TIMEOUT', 60, logger)
+
+    # The client timeout shouldn't be shorter than the server timeout
+    # https://kopf.readthedocs.io/en/latest/configuration/#api-timeouts
+    if client_timeout < server_timeout:
+        logger.warning(f"The client timeout ({client_timeout}) is shorter than the server timeout ({server_timeout}). Consider increasing the client timeout to be higher")
 
     # Set k8s event logging
     settings.posting.enabled = get_env_var_bool('EVENT_LOGGING')
 
     # Create the folder from which we will write/delete files
-    create_folder(FOLDER, logger)
+    create_folder(folder, logger)
 
     if get_env_var_bool('UNIQUE_FILENAMES'):
         logger.info("Unique filenames will be enforced.")
@@ -25,16 +41,19 @@ def startup_tasks(settings: kopf.OperatorSettings, logger, **_):
 @kopf.on.resume('', 'v1', 'configmaps', labels={LABEL: kopf.PRESENT})
 @kopf.on.create('', 'v1', 'configmaps', labels={LABEL: kopf.PRESENT})
 @kopf.on.update('', 'v1', 'configmaps', labels={LABEL: kopf.PRESENT})
-def write_configmap(body, event, logger, **_):
-    write_file(event, body, logger)
-
 @kopf.on.resume('', 'v1', 'secrets', labels={LABEL: kopf.PRESENT})
 @kopf.on.create('', 'v1', 'secrets', labels={LABEL: kopf.PRESENT})
 @kopf.on.update('', 'v1', 'secrets', labels={LABEL: kopf.PRESENT})
-def write_secret(body, event, logger, **_):
-    write_file(event, body, logger)
+async def cru_fn(body, event, logger, **_):
+    try:
+        await write_file(event, body, logger)
+    except asyncio.CancelledError:
+        logger.info(f"Write file cancelled for {body['kind']}")
 
 @kopf.on.delete('', 'v1', 'configmaps', labels={LABEL: kopf.PRESENT})
 @kopf.on.delete('', 'v1', 'secrets', labels={LABEL: kopf.PRESENT})
-def delete_fn(body, logger, **_):
-    delete_file(body, logger)
+async def delete_fn(body, logger, **_):
+    try:
+        await delete_file(body, logger)
+    except asyncio.CancelledError:
+        logger.info(f"Delete file cancelled for {body['kind']}")
