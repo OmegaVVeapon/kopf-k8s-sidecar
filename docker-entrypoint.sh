71f0d850dd1a64efe61c7b614d9ef1cd9f90a0d2
Alan Silva <alansq16@gmail.com>
2020-11-30 15:37:54 -0500

    Adding liveness probe
    - Liveness probe will now be enabled by default
    - Added log line when filtering by namespace since kopf doesn't print
    one

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,9 @@ if [ "$1" = 'kopf-k8s-sidecar' ]; then
 
     [[ "$VERBOSE" = "true" ]] && command+=("--verbose")
     [[ "$DEBUG" = "true" ]] && command+=("--debug")
-    [ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && command+=("--namespace=${NAMESPACE}")
+    [ -n "$NAMESPACE" ] && [ "$NAMESPACE" != "ALL" ] && echo "Only watching resources from the ${NAMESPACE} namespace" && command+=("--namespace=${NAMESPACE}")
+
+    [[ "$LIVENESS" = "false" ]] && echo "Liveness /healthz endpoint has been explicitely disabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
 
     exec "${command[@]}"
 fi
