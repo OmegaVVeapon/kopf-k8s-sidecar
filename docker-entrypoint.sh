3e9af4c6a05ea8f5619cf021c74bd8e952159553
Alan Silva <alansq16@gmail.com>
2021-01-23 14:39:50 -0500

    Setting the USER variable at runtime
    - Since we won't know what security context we're running on until the
    container is started, setting the USER at runtime.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,6 +11,12 @@ if [ "$1" = 'kopf-k8s-sidecar' ]; then
 
     [[ "$LIVENESS" = "false" ]] && echo "Liveness /healthz endpoint has been explicitely disabled!" || command+=("--liveness=http://0.0.0.0:8080/healthz")
 
+    # Fix https://github.com/OmegaVVeapon/kopf-k8s-sidecar/issues/12 by setting the USER env var to prevent python from looking for a matching uid/gid in the password database.
+    # See https://github.com/python/cpython/blob/v3.6.0/Lib/getpass.py#L155-L170.
+    USER=$(id --user)
+    echo "Setting USER environment variable to ${USER}"
+    export USER=$USER
+
     exec "${command[@]}"
 fi
 
