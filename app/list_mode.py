99317da0b6f7ab3ad55cba5ae674d29bd55d1021
Alan Silva <alansq16@gmail.com>
2021-06-22 00:01:14 -0400

    Using tenacity to add retries for connection issues in LIST mode

--- app/list_mode.py
+++ app/list_mode.py
@@ -1,49 +1,57 @@
-import sys
 import logging
-import functools
 import pykube
 from io_helpers import write_file
-from conditions import label_is_satisfied
-from time import sleep
+from tenacity import retry, stop_after_attempt, wait_fixed
 import sidecar_settings
 
 logger = logging.getLogger(__name__)
 
-def retry(retry_count=5, delay=5, allowed_exceptions=()):
-    def decorator(f):
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            for _ in range(retry_count):
-                try:
-                    result = f(*args, **kwargs)
-                    if result:
-                        return result
-                except allowed_exceptions as e:
-                    pass
-                logger.info("Waiting for %s seconds before trying again", delay)
-                sleep(delay)
-            logger.error("Failed the maximum of %d times... Exiting with error", retry_count)
-            sys.exit(1)
-        return wrapper
-    return decorator
+def show_attempts_and_sleep_time(retry_state):
+    logger.info('Retrying %s: attempt %s ended with: %s',
+        retry_state.fn, retry_state.attempt_number, retry_state.outcome)
+    logger.info("Waiting 2 seconds to retry...")
 
-@retry(retry_count=5, delay=5)
-def _get_configmaps(namespace):
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_fixed(2),
+    before_sleep=show_attempts_and_sleep_time
+)
+def _get_configmaps(namespace, label):
     try:
         api = pykube.HTTPClient(pykube.KubeConfig.from_env())
-        configmaps = pykube.ConfigMap.objects(api).filter(namespace=namespace)
-        return configmaps
-    except pykube.exceptions.HTTPError as e:
+        configmaps = pykube.ConfigMap.objects(api).filter(
+            namespace=namespace,
+            selector=label
+        )
+    except pykube.exceptions.HTTPError as pykube_http_error:
         logger.exception("The connection to the Kubernetes API server has failed!")
+        raise pykube_http_error
+    except Exception:
+        logger.exception("Unexpected non-HTTP exception...")
+        raise
+    else:
+        return configmaps
 
-@retry(retry_count=5, delay=2)
-def _get_secrets(namespace):
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_fixed(2),
+    before_sleep=show_attempts_and_sleep_time
+)
+def _get_secrets(namespace, label):
     try:
         api = pykube.HTTPClient(pykube.KubeConfig.from_env())
-        configmaps = pykube.Secret.objects(api).filter(namespace=namespace)
-        return configmaps
-    except pykube.exceptions.HTTPError as e:
+        secrets = pykube.Secret.objects(api).filter(
+            namespace=namespace,
+            selector=label
+        )
+    except pykube.exceptions.HTTPError as pykube_http_error:
         logger.exception("The connection to the Kubernetes API server has failed!")
+        raise pykube_http_error
+    except Exception:
+        logger.exception("Unexpected non-HTTP exception...")
+        raise
+    else:
+        return secrets
 
 def one_run():
     """Search through all the ConfigMaps and Secrets in the specified namespaces. If they meet the label requirements,
@@ -54,18 +62,36 @@ def one_run():
     else:
         namespaces = sidecar_settings.NAMESPACE
 
+    if sidecar_settings.LABEL_VALUE:
+        label = {sidecar_settings.LABEL: sidecar_settings.LABEL_VALUE}
+    else:
+        label = sidecar_settings.LABEL
+
     if sidecar_settings.RESOURCE in ('configmap', 'both'):
-        logger.info("Looking for configmap resources with label %s and value %s", sidecar_settings.LABEL, sidecar_settings.LABEL_VALUE)
+        logger.info("Looking for configmaps...")
 
         for namespace in namespaces:
-            configmaps = _get_configmaps(namespace)
+            configmaps = _get_configmaps(namespace, label)
+
+            if not configmaps:
+                logger.info("No configmaps found with label %s", label)
+
             for configmap in configmaps:
-                if label_is_satisfied(configmap.obj['metadata']):
-                    write_file("create", configmap.obj, configmap.kind, logger)
+                write_file("create", configmap.obj, configmap.kind, logger)
 
     if sidecar_settings.RESOURCE in ('secret', 'both'):
+        logger.info("Looking for secrets...")
+
         for namespace in namespaces:
-            secrets = _get_secrets(namespace)
+            secrets = _get_secrets(namespace, label)
+
+            if not secrets:
+                logger.info("No secrets found with label %s", label)
+
             for secret in secrets:
-                if label_is_satisfied(secret.obj['metadata']):
-                    write_file("create", secret.obj, secret.kind, logger)
+                write_file("create", secret.obj, secret.kind, logger)
+
+    if not configmaps and not secrets:
+        logger.warning("Could not find configmaps OR secrets matching label %s. Was this intended?", label)
+
+    logger.info("LIST mode completed. Exiting...")
